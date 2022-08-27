1ab68d5229a7770502ef60795fe7d2fa3cee82c3
Rakhchik <ilyachannle@gmail.com>
2022-08-28 01:05:32 +0300

    Created Listener that open Activity D and clear that Stack

--- app/src/main/java/ru/rakhcheev/task/activities/ActivityC.kt
+++ app/src/main/java/ru/rakhcheev/task/activities/ActivityC.kt
@@ -15,6 +15,11 @@ class ActivityC : AppCompatActivity() {
         findViewById<Button>(R.id.open_a_btn).setOnClickListener {
             startActivity(Intent(this, ActivityA::class.java))
         }
+
+        findViewById<Button>(R.id.open_d_btn).setOnClickListener {
+            startActivity(Intent(this, ActivityD::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK))
+        }
+
     }
 
     override fun onStart() {
