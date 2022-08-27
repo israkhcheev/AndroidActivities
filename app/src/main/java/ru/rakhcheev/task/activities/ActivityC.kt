96c9973ab579669106bd909cfc2876794a870404
Rakhchik <ilyachannle@gmail.com>
2022-08-28 01:04:29 +0300

    Created Listener that open existing instance Activity A and run onNewIntent

---
+++ app/src/main/java/ru/rakhcheev/task/activities/ActivityC.kt
@@ -0,0 +1,35 @@
+package ru.rakhcheev.task.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityC : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+
+        findViewById<Button>(R.id.open_a_btn).setOnClickListener {
+            startActivity(Intent(this, ActivityA::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("C", "C ACTIVITY \tonStart  - $this")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("C", "C ACTIVITY \tonResume - $this")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("C", "C ACTIVITY \tonDestroy  - $this")
+    }
+}
