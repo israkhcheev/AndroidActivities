d45c43133c69ed1845c11f9068f8332c3e3b34ee
Rakhchik <ilyachannle@gmail.com>
2022-08-29 00:09:28 +0300

    Create SenderActivity and setup layout

---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/activity/SenderActivity.kt
@@ -0,0 +1,16 @@
+package otus.gpb.homework.activities.sender.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.sender.R
+
+class SenderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sender_activity_layout)
+    }
+}
