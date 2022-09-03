aa5ad4b4a4025808483e8e555735d942c0a8439e
Rakhchik <ilyachannle@gmail.com>
2022-09-03 13:28:09 +0300

    до конца разобрался, как работает taskAffinity

--- app/src/main/java/ru/rakhcheev/task/activities/ActivityB.kt
+++ app/src/main/java/ru/rakhcheev/task/activities/ActivityB.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ActivityB : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
